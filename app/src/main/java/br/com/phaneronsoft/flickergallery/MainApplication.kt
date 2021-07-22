1541013496fad2481501a3d0895b5b2588fa192a
Altieres Bianchi <altieres@phaneronsoft.com.br>
2021-07-22 20:12:54 -0300

    Adicionado Koin logger Level

--- app/src/main/java/br/com/phaneronsoft/flickergallery/MainApplication.kt
+++ app/src/main/java/br/com/phaneronsoft/flickergallery/MainApplication.kt
@@ -7,6 +7,7 @@ import br.com.phaneronsoft.flickergallery.di.mainViewModelModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
 
 class MainApplication : Application() {
     override fun onCreate() {
@@ -20,7 +21,7 @@ class MainApplication : Application() {
             androidContext(this@MainApplication)
 
             // Use AndroidLogger as Koin Logger - default Level.INFO
-            androidLogger()
+            androidLogger(Level.ERROR)
 
             modules(
                 listOf(
